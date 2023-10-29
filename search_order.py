e8ac213247a3b89a695efa20f76a8c576c037031
opervakov2001 <146844589+opervakov2001@users.noreply.github.com>
2023-10-29 14:55:11 +0400

    Update search_order.py

--- search_order.py
+++ search_order.py
@@ -1,8 +1,8 @@
 #Олег Перваков-дипломный проект-11 спринт-инженер по тестированию плюс
-import Create_and_Search_Order
+import Create_Order
 
 def test_search_order_status_code_200():
-    assert Create_and_Search_Order.search_order().status_code == 200
+    assert Create_Order.search_order().status_code == 200
 
 
 
