f7c4fd1d8e4e0ee91b79cc9a12a3380ec10346f9
opervakov2001 <146844589+opervakov2001@users.noreply.github.com>
2023-10-29 14:57:28 +0400

    Delete search_order.py

--- search_order.py
+++
@@ -1,8 +0,0 @@
-#Олег Перваков-дипломный проект-11 спринт-инженер по тестированию плюс
-import Create_Order
-
-def test_search_order_status_code_200():
-    assert Create_Order.search_order().status_code == 200
-
-
-
